06d2ddc1184228124a65eaea47447b36a08347cd
安藤春香 <haruan2394@gmail.com>
2018-12-14 19:50:20 +0900

    Add: require spec_helper

--- spec/tapiron_spec.rb
+++ spec/tapiron_spec.rb
@@ -1,9 +1,6 @@
+require "spec_helper"
 RSpec.describe Tapiron do
   it "has a version number" do
     expect(Tapiron::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
