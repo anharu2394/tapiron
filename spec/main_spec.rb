5d1af5250760e8da1cb49efa5efafc258f4699b3
安藤春香 <haruan2394@gmail.com>
2018-12-16 09:09:39 +0900

    Add: test for router

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,8 +1,40 @@
 require "spec_helper"
 RSpec.describe Tapiron::Main do
-  it "can start server" do
-    router = {}
-    Tapiron::Main.prepare({}).cook()
-    expect(last_response).to be nil
+  context "unset routes" do
+    let(:app) { 
+      router = Tapiron::Router.new
+      Tapiron::Main.prepare(router) 
+    }
+    it "can start server" do
+      get '/'
+      expect(last_response.status).to eq 404
+    end
+  end
+  context "set routes" do
+    let(:app) { 
+      router = Tapiron::Router.new
+      router.get("/", -> { "Hello World" })
+      router.get("/hello", -> { "Hello" })
+      Tapiron::Main.prepare(router) 
+    }
+    it "can set route get request" do
+      response = get "/"
+      expect(response.status).to eq 200
+      expect(response.body).to eq "Hello World"
+    end
+    it "can set route other get request" do
+      response = get "/hello"
+      expect(response.status).to eq 200
+      expect(response.body).to eq "Hello"
+    end
+    it "can set route other get request include /" do
+      response = get "/hello/"
+      expect(response.status).to eq 200
+      expect(response.body).to eq "Hello"
+    end
+    it "can set route get request" do
+      response = post "/"
+      expect(response.status).to eq 404
+    end
   end
 end
