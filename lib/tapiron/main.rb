a7d4c129b2ac0b07302ce382a396c8ef669eef0d
Anharu <haruan2394@gmail.com>
2019-03-03 15:19:01 +0900

    Add: main format

--- lib/tapiron/main.rb
+++ lib/tapiron/main.rb
@@ -1,21 +1,26 @@
+# frozen_string_literal: true
+
 require 'rack'
 class Tapiron::Main
   def initialize(router)
     @router = router
   end
+
   def self.prepare(router)
-    self.new(router)
+    new(router)
   end
+
   def cook
     Rack::Handler::WEBrick.run self
   end
+
   def call(env)
     req = Rack::Request.new(env)
     @router.routes.each do |path, res|
-      if (req.path_info == path || req.path_info == path + "/")&& req.request_method == res[:request_method]
-        return [200, {'Content-Type' => 'text/plain'}, [res[:response]]]
+      if (req.path_info == path || req.path_info == path + '/') && req.request_method == res[:request_method]
+        return [200, { 'Content-Type' => 'text/plain' }, [res[:response]]]
       end
     end
-    [404, {'Content-Type' => 'text/plain'}, ['404 Not Found']]
+    [404, { 'Content-Type' => 'text/plain' }, ['404 Not Found']]
   end
 end
