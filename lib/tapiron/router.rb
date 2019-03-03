961dbd81702a72ad60b7cc8b52a6d56774ffdaf8
Anharu <haruan2394@gmail.com>
2019-03-03 15:22:54 +0900

    Add: router format

--- lib/tapiron/router.rb
+++ lib/tapiron/router.rb
@@ -1,9 +1,12 @@
-class Tapiron::Router 
+# frozen_string_literal: true
+
+class Tapiron::Router
   attr_reader :routes
   def initialize
     @routes = {}
   end
+
   def get(path, func)
-    @routes[path] = { request_method: "GET", response:func[]}
+    @routes[path] = { request_method: 'GET', response: func[] }
   end
 end
